57887c5386ddcadbb0a45f8fa2f6d2525d39e29e
sarahcrox <croxford.sarahm@gmail.com>
2020-02-17 20:30:03 -0700

    Age Calculator Completed

---
+++ calc.js
@@ -0,0 +1,6 @@
+alert("CONNECTED!")
+alert("Use this Calculator!");
+var age = prompt("How old are you?");
+var daysAlive = age * 365
+
+console.log("You have been alive for " + daysAlive + " days!");ls
